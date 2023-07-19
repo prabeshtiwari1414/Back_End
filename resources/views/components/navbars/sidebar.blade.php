1fb7d2b6483c9e7443254d5ddbe9407bc56ffc36
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2023-07-19 11:51:35 +0545

    gallery

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -61,7 +61,7 @@
                 <a class="nav-link text-white {{ $activePage == 'gallery' ? ' active bg-gradient-primary' : '' }} "
                     href="{{route('getAddGallery') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i style="font-size: 1rem;" class="fas fa-lg fa-list-ul ps-2 pe-2 text-center"></i>
+                        <i class="material-icons opacity-10">dashboard</i>
                     </div>
                     <span class="nav-link-text ms-1">Gallery</span>
                 </a>
